f4440fd92ac766e7e672e47d35bb1b51efc0c662
Alexandre Mak <amak@c2r10s7.42lisboa.com>
2022-11-23 02:10:29 +0000

    ft_printf draft

---
+++ printf/ft_printf.c
@@ -0,0 +1,19 @@
+
+#include "libftprintf.h"
+
+int	ft_printf(const char *input, ...)
+{
+	va_list	arguments;
+
+	va_start(arguments, input);
+	va_arg
+
+	if (valid_input(input))
+
+	else
+		return (0);
+
+
+}
+
+// cspdiuxX%
---
+++ printf/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{  
+  int x;
+  
+  x = 18;
+  printf("%d", printf("%d\n", x));
+  return 0;
+}
