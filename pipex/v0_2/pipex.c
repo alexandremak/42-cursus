0a37e1f51126cbc6a8d0d918a9de10f475e67b8c
Alexandre Mak <amak@c2r9s3.42lisboa.com>
2023-03-07 22:58:42 +0000

    new version pipex tests files

---
+++ pipex/v0_2/pipex.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amak <amak@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/26 16:25:30 by amak              #+#    #+#             */
+/*   Updated: 2023/03/07 22:49:59 by amak             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+int	main(int argc, char **argv, char **envp)
+{
+	char	*output;
+	char	**paths;
+
+	if (argc != 5)
+		printf("wrong arguments!\n");
+	printf("%s\n", argv[0]);
+	output = path_strg(envp);
+	printf("%s\n", output);
+	paths = ft_split(output, ':');
+	while (*paths)
+	{
+		printf("%s\n", *paths);
+		paths++;
+	}
+}
+
+
+// gcc -Wall -Wextra -Werror pipex.c pipex_utils.c ft_calloc.c ft_memcpy.c ft_putstr.c ft_split.c ft_strdup.c ft_strjoin.c ft_strlcpy.c ft_strlen.c ft_strncmp.c ft_substr.c -o pipex
---
+++ pipex/v0_2/pipex.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amak <amak@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/28 19:59:56 by amak              #+#    #+#             */
+/*   Updated: 2023/03/07 22:22:03 by amak             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+
+void	*ft_calloc(size_t nmemb, size_t size);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	ft_putstr(char *s);
+char	**ft_split(char const *s, char c);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *str);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*path_strg(char **envp);
+
+#endif
